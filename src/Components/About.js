71273cf4329142bd94806a9f4bf2a99bd7fac4ed
Mehul <71012892+M-Destiny@users.noreply.github.com>
2022-12-06 17:58:27 +0530

    06-12-2022 Highchart Implemetation start created marksand percentage calculator

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,46 @@
+import Graph from "./Graph";
+import { useState } from "react";
 export default function About() {
+
+  const [physics, setPhysics] = useState(0);
+  const [chemistry, setChemistry] = useState(0);
+  const [maths, setMaths] = useState(0);
+  const [data, setData] = useState({});
+
+  function phy(e) {
+    // console.log(e.target.value);
+    setPhysics(e.target.value);
+  }
+  function chm(e) {
+    // console.log(e.target.value);
+    setChemistry(e.target.value);
+  }
+  function mts(e) {
+    // console.log(e.target.value);
+    setMaths(e.target.value);
+  }
+  function myfunc() {
+    var total = parseInt(physics) + parseInt(chemistry) + parseInt(maths);
+    var percentage = (total / 300) * 100;
+    console.log(percentage);
+    console.log(total);
+    // console.log('object');
+    setData({ a: physics, b: chemistry, c: maths, d: total, e: percentage })
+    console.log(data);
+
+
+
+  }
   return (
-    <div>About</div>
+    <div className="container">
+
+      <input type="text" placeholder="Physics" className="form-control mb-2" onChange={phy} />
+      <input type="text" placeholder="Chemistry" className="form-control mb-2" onChange={chm} />
+      <input type="text" placeholder="Maths" className="form-control mb-2" onChange={mts} />
+      <button className="btn btn-primary mt-2" onClick={myfunc}>Submit</button>
+      {/* {physics}{chemistry}{maths} */}
+
+      <Graph result={data} />
+    </div>
   )
 }
---
+++ src/Components/Graph.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// import HighCharts from 'highcharts';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+export default function Graph(props) {
+    // console.log(props)
+    return (
+        <div className='container'>
+            <h1>Records</h1>
+            <table className='table'>
+                <tr>
+                    <th scope='col'>Physics</th>
+                    <th scope='col'>Chemistry</th>
+                    <th scope='col'>Maths</th>
+                </tr>
+                <tr>
+                    <td>{props.result.a}</td>
+                    <td>{props.result.b}</td>
+                    <td>{props.result.c}</td>
+                </tr>
+            </table>
+            <p>
+                <b>Total marks :{props.result.d} / 300</b>
+                <br />
+                <b>Percentage: {props.result.e}  % </b>
+
+            </p>
+        </div>
+    )
+}
